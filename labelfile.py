4ea5a5990b57030a0cf3ae52d1224452f8a51f9c
Meirin Oan Evans <meirin.oan.evans@cern.ch>
2020-01-02 10:07:37 +0000

    Unified plotting code

---
+++ labelfile.py
@@ -0,0 +1,4 @@
+variable_labels = {'met_et':r'$\mathrm{E_{T}^{miss}}$ [GeV]',
+                      'mllll':r'$\mathrm{m_{4l}}$ [GeV]',
+                      'mll':r'$\mathrm{m_{ll}}$ [GeV]',
+                      'myy':r'$\mathrm{m_{\gamma\gamma}}$ [GeV]'}
